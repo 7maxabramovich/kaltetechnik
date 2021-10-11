c1e11d87ade394798f82f28f0a66baea91cafe53
7maxabramovich <7maxabramovich@gmail.com>
2021-10-12 01:43:14 +0300

    all

---
+++ js/js.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+	$('.slick-slider').slick({
+		autoplay: false,
+		speed: 800,
+		autoplaySpeed: 2000,
+		arrows: true,
+		dots: true,
+		adaptiveHeight: true,
+		infinite: true,
+		slidesToShow: 1, 
+		slidesToScroll: 1,
+	});
+});
